0f1e0a04e2e9c235d61d5b7be87e6914d936c3f5
Daniel Ferreira <2018.itm.danielrodrigues@gmail.com>
2021-03-30 20:03:09 +0100

    Calendar update

--- lib/screens/interface/class_screen.dart
+++ lib/screens/interface/class_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class ClassScreen extends StatefulWidget {
   ClassScreen({Key key}) : super(key: key);
@@ -8,6 +9,15 @@ class ClassScreen extends StatefulWidget {
 }
 
 class _ClassScreenState extends State<ClassScreen> {
+  CalendarController _controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = CalendarController();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -17,13 +27,55 @@ class _ClassScreenState extends State<ClassScreen> {
             Row(
               children: [
                 Padding(
-                  padding: EdgeInsets.only(left: 20, top: 30),
+                  padding: EdgeInsets.only(left: 20, top: 30, bottom: 30),
                   child: Image.asset(
                     'assets/logo.png',
                     width: 220,
                   ),
                 ),
               ],
+            ),
+            TableCalendar(
+              initialCalendarFormat: CalendarFormat.month,
+              calendarStyle: CalendarStyle(
+                todayColor: Colors.blue, //? marca o dia de hoje
+                selectedColor: Colors.yellow, //? marca o dia selecionado
+                outsideWeekendStyle: //? propriedades dos dias do fim-de-semana
+                    TextStyle(color: Colors.blue[200], fontFamily: 'Ubuntu'),
+                outsideStyle:
+                    TextStyle(color: Colors.blue[100]), //? dias do mes seguinte
+                weekdayStyle:
+                    TextStyle(fontFamily: 'Ubuntu'), //? dias da semana
+                weekendStyle: //? fins de semana
+                    TextStyle(fontFamily: 'Ubuntu', color: Colors.blue),
+                contentPadding: EdgeInsets.only(
+                    top: 20), //? espaço entre o mes e calendario
+              ),
+              daysOfWeekStyle: DaysOfWeekStyle(
+                weekdayStyle: TextStyle(
+                    fontFamily: 'Ubuntu',
+                    color: Colors.grey), //? segunda a sexta
+                weekendStyle: TextStyle(
+                    fontFamily: 'Ubuntu',
+                    color: Colors.blue[900]), //? sabado e domingo
+              ),
+              headerStyle: HeaderStyle(
+                //? propriedades do header
+                titleTextStyle: TextStyle(fontFamily: 'Ubuntu', fontSize: 20),
+                formatButtonDecoration: BoxDecoration(
+                  //? caixa display do calendario
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                formatButtonTextStyle: TextStyle(
+                  //? texto dentro da caixa
+                  fontFamily: 'Ubuntu',
+                  color: Colors.white,
+                ),
+                formatButtonShowsNext: false,
+              ),
+              startingDayOfWeek: StartingDayOfWeek.monday,
+              calendarController: _controller,
             ),
           ],
         ),
